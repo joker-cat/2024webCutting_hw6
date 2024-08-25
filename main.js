93a5a9fe913236ecb83899185123dfebff4d6698
joker-cat <jocker.ss.cat@gmail.com>
2024-08-25 20:06:22 +0800

    swiper

--- main.js
+++ main.js
@@ -1,4 +1,61 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+// init Swiper:
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay, Scrollbar } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination, Autoplay],
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  // Default parameters
+  // slidesPerView: 1,
+  // spaceBetween: 48,
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    996: {
+      slidesPerView: 2,
+      spaceBetween: 48
+    },
+    // when window width is >= 480px
+    // 480: {
+    //   slidesPerView: 3,
+    //   spaceBetween: 30
+    // },
+    // // when window width is >= 640px
+    // 640: {
+    //   slidesPerView: 4,
+    //   spaceBetween: 40
+    // }
+  }
+});
+
 console.log("Hello world!");

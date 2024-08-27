efdc82c9a3083284c94ea1ae87ed070140fbbe1a
joker <jocker.ss.cat@gmail.com>
2024-08-27 23:39:05 +0800

    fin: article

--- main.js
+++ main.js
@@ -16,24 +16,26 @@ const swiper = new Swiper('.swiper', {
   // configure Swiper to use modules
   modules: [Navigation, Pagination, Autoplay],
   loop: true,
+  type: 'custom',
   autoplay: {
     delay: 3000,
   },
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: "fraction",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
   // Default parameters
   // slidesPerView: 1,
   // spaceBetween: 48,
